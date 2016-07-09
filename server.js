00d12cb7d7419d232fa51a809dacdcfc5027df95
Zach Russell <zachatrocity@gmail.com>
2016-07-09 11:45:34 -0400

    added express server

---
+++ server.js
@@ -0,0 +1,19 @@
+var express =  require('express')
+var logger     = require('morgan')
+
+
+var app = express()
+app.use(logger('dev'))
+
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/resources', express.static(__dirname + '/resoures'));
+app.use('/html', express.static(__dirname + '/html'));
+app.use('/css', express.static(__dirname + '/css'));
+
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/html/index.html');
+});
+
+var server = app.listen(1337, function () {
+  console.log('server listening on %d', server.address().port)
+})
